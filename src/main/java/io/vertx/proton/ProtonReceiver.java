925895591a09783b53a59d96618e5a26f8f5a5b4
Tim Fox <timvolpe@gmail.com>
2015-10-21 14:34:56 +0100

    Remove some more stuff in receiver

--- src/main/java/io/vertx/proton/ProtonReceiver.java
+++ src/main/java/io/vertx/proton/ProtonReceiver.java
@@ -4,16 +4,8 @@ package io.vertx.proton;
  * @author <a href="http://tfox.org">Tim Fox</a>
  */
 public interface ProtonReceiver {
-
-  int recv(byte[] bytes, int offset, int size);
-
-  ProtonReceiver drain(int credit);
-
+  
   ProtonReceiver flow(int credits);
-
-  boolean draining();
-
-  ProtonReceiver setDrain(boolean drain);
 
   ProtonReceiver handler(ProtonMessageHandler handler);
 }
