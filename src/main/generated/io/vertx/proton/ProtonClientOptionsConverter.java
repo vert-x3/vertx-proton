9cf7607ba51f0e5cd73b1df973ea3c64451b8cbd
Robbie Gemmell <robbie.gemmell@gmail.com>
2018-06-01 17:10:07 +0100

    commit latest generated file updates

--- src/main/generated/io/vertx/proton/ProtonClientOptionsConverter.java
+++ src/main/generated/io/vertx/proton/ProtonClientOptionsConverter.java
@@ -1,19 +1,3 @@
-/*
- * Copyright (c) 2014 Red Hat, Inc. and others
- *
- * Red Hat licenses this file to you under the Apache License, version 2.0
- * (the "License"); you may not use this file except in compliance with the
- * License.  You may obtain a copy of the License at:
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
- * License for the specific language governing permissions and limitations
- * under the License.
- */
-
 package io.vertx.proton;
 
 import io.vertx.core.json.JsonObject;
@@ -21,33 +5,50 @@ import io.vertx.core.json.JsonArray;
 
 /**
  * Converter for {@link io.vertx.proton.ProtonClientOptions}.
- *
- * NOTE: This class has been automatically generated from the {@link io.vertx.proton.ProtonClientOptions} original class using Vert.x codegen.
+ * NOTE: This class has been automatically generated from the {@link "io.vertx.proton.ProtonClientOptions} original class using Vert.x codegen.
  */
  class ProtonClientOptionsConverter {
 
-   static void fromJson(JsonObject json, ProtonClientOptions obj) {
-    if (json.getValue("enabledSaslMechanisms") instanceof JsonArray) {
-      json.getJsonArray("enabledSaslMechanisms").forEach(item -> {
-        if (item instanceof String)
-          obj.addEnabledSaslMechanism((String)item);
-      });
-    }
-    if (json.getValue("heartbeat") instanceof Number) {
-      obj.setHeartbeat(((Number)json.getValue("heartbeat")).intValue());
-    }
-    if (json.getValue("maxFrameSize") instanceof Number) {
-      obj.setMaxFrameSize(((Number)json.getValue("maxFrameSize")).intValue());
-    }
-    if (json.getValue("sniServerName") instanceof String) {
-      obj.setSniServerName((String)json.getValue("sniServerName"));
-    }
-    if (json.getValue("virtualHost") instanceof String) {
-      obj.setVirtualHost((String)json.getValue("virtualHost"));
+   static void fromJson(Iterable<java.util.Map.Entry<String, Object>> json, ProtonClientOptions obj) {
+    for (java.util.Map.Entry<String, Object> member : json) {
+      switch (member.getKey()) {
+        case "enabledSaslMechanisms":
+          if (member.getValue() instanceof JsonArray) {
+            ((Iterable<Object>)member.getValue()).forEach( item -> {
+              if (item instanceof String)
+                obj.addEnabledSaslMechanism((String)item);
+            });
+          }
+          break;
+        case "heartbeat":
+          if (member.getValue() instanceof Number) {
+            obj.setHeartbeat(((Number)member.getValue()).intValue());
+          }
+          break;
+        case "maxFrameSize":
+          if (member.getValue() instanceof Number) {
+            obj.setMaxFrameSize(((Number)member.getValue()).intValue());
+          }
+          break;
+        case "sniServerName":
+          if (member.getValue() instanceof String) {
+            obj.setSniServerName((String)member.getValue());
+          }
+          break;
+        case "virtualHost":
+          if (member.getValue() instanceof String) {
+            obj.setVirtualHost((String)member.getValue());
+          }
+          break;
+      }
     }
   }
 
    static void toJson(ProtonClientOptions obj, JsonObject json) {
+    toJson(obj, json.getMap());
+  }
+
+   static void toJson(ProtonClientOptions obj, java.util.Map<String, Object> json) {
     if (obj.getEnabledSaslMechanisms() != null) {
       JsonArray array = new JsonArray();
       obj.getEnabledSaslMechanisms().forEach(item -> array.add(item));
--- src/main/generated/io/vertx/proton/ProtonTransportOptionsConverter.java
+++ src/main/generated/io/vertx/proton/ProtonTransportOptionsConverter.java
@@ -1,19 +1,3 @@
-/*
- * Copyright (c) 2014 Red Hat, Inc. and others
- *
- * Red Hat licenses this file to you under the Apache License, version 2.0
- * (the "License"); you may not use this file except in compliance with the
- * License.  You may obtain a copy of the License at:
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
- * License for the specific language governing permissions and limitations
- * under the License.
- */
-
 package io.vertx.proton;
 
 import io.vertx.core.json.JsonObject;
@@ -21,21 +5,32 @@ import io.vertx.core.json.JsonArray;
 
 /**
  * Converter for {@link io.vertx.proton.ProtonTransportOptions}.
- *
- * NOTE: This class has been automatically generated from the {@link io.vertx.proton.ProtonTransportOptions} original class using Vert.x codegen.
+ * NOTE: This class has been automatically generated from the {@link "io.vertx.proton.ProtonTransportOptions} original class using Vert.x codegen.
  */
  class ProtonTransportOptionsConverter {
 
-   static void fromJson(JsonObject json, ProtonTransportOptions obj) {
-    if (json.getValue("heartbeat") instanceof Number) {
-      obj.setHeartbeat(((Number)json.getValue("heartbeat")).intValue());
-    }
-    if (json.getValue("maxFrameSize") instanceof Number) {
-      obj.setMaxFrameSize(((Number)json.getValue("maxFrameSize")).intValue());
+   static void fromJson(Iterable<java.util.Map.Entry<String, Object>> json, ProtonTransportOptions obj) {
+    for (java.util.Map.Entry<String, Object> member : json) {
+      switch (member.getKey()) {
+        case "heartbeat":
+          if (member.getValue() instanceof Number) {
+            obj.setHeartbeat(((Number)member.getValue()).intValue());
+          }
+          break;
+        case "maxFrameSize":
+          if (member.getValue() instanceof Number) {
+            obj.setMaxFrameSize(((Number)member.getValue()).intValue());
+          }
+          break;
+      }
     }
   }
 
    static void toJson(ProtonTransportOptions obj, JsonObject json) {
+    toJson(obj, json.getMap());
+  }
+
+   static void toJson(ProtonTransportOptions obj, java.util.Map<String, Object> json) {
     json.put("heartbeat", obj.getHeartbeat());
     json.put("maxFrameSize", obj.getMaxFrameSize());
   }
